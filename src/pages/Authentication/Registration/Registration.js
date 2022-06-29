d6acb5068e51f3b5a9807d7c9627debf0fd1ebb7
Avijit Roy <royavijit782@gmail.com>
2022-06-30 05:39:41 +0600

    login, signup, notfound page layout sizing updatation has done

--- src/pages/Authentication/Registration/Registration.js
+++ src/pages/Authentication/Registration/Registration.js
@@ -51,50 +51,48 @@ const Registration = () => {
     return (
         <>
             <HeaderForRestOfPages></HeaderForRestOfPages>
-            <div className="registration-bg text-dark">
-                <div className="registration-page d-flex justify-content-center align-items-center align-items-lg-start">
-                    <div className="container p-4 p-lg-0 registration-content">
-                        <div className="row g-5">
-                            <div className="col-12 col-lg-6">
-                                <div className="registration-content-sizing">
-                                    <div className="d-flex justify-content-between mb-5">
-                                        <NavLink to="/login" className="text-decoration-none text-dark py-2 text-center w-100" activeStyle={{ backgroundColor: "rgb(207, 206, 199)" }}>Log In</NavLink>
-                                        <NavLink to="/register" className="text-decoration-none text-dark py-2 text-center w-100" activeStyle={{ backgroundColor: "rgb(207, 206, 199)" }}>Registration</NavLink>
+            <div id='registration' className="registration-bg text-dark d-flex justify-content-center align-items-center align-items-lg-start">
+                <div className="container">
+                    <div className="row g-5">
+                        <div className="col-12 col-lg-6">
+                            <div className="registration-content-sizing">
+                                <div className="d-flex justify-content-between mb-5">
+                                    <NavLink to="/login" className="text-decoration-none text-dark py-2 text-center w-100" activeStyle={{ backgroundColor: "rgb(207, 206, 199)" }}>Log In</NavLink>
+                                    <NavLink to="/register" className="text-decoration-none text-dark py-2 text-center w-100" activeStyle={{ backgroundColor: "rgb(207, 206, 199)" }}>Registration</NavLink>
+                                </div>
+                                <form onSubmit={handleRegistration} className="px-3">
+                                    <div className="mb-3">
+                                        <label htmlFor="exampleInputName" className="form-label fs-6">Name</label>
+                                        <input type="text" onBlur={getName} className="form-control registration-input-bg py-3 rounded-0" id="exampleInputName" placeholder="Name" required />
                                     </div>
-                                    <form onSubmit={handleRegistration} className="px-3">
-                                        <div className="mb-3">
-                                            <label htmlFor="exampleInputName" className="form-label fs-6">Name</label>
-                                            <input type="text" onBlur={getName} className="form-control registration-input-bg py-3 rounded-0" id="exampleInputName" placeholder="Name" required />
+                                    <div className="mb-3">
+                                        <label htmlFor="exampleInputEmail1" className="form-label fs-6">Email address</label>
+                                        <input type="email" onBlur={getEmail} className="form-control registration-input-bg py-3 rounded-0" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="abc@pqr.xyz" required />
+                                        <div id="emailHelp" className="form-text text-dark">We'll never share your email with anyone else.</div>
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="inputPassword5" className="form-label fs-6">Password</label>
+                                        <input type="password" onBlur={getPassword} id="inputPassword5" className="form-control registration-input-bg py-3 rounded-0" aria-describedby="passwordHelpBlock" placeholder="Password" required />
+                                        <div id="passwordHelpBlock" className="form-text text-dark">
+                                            Your password must be at least 6 characters long, contain at least one lowercase letter and at least a number.
                                         </div>
-                                        <div className="mb-3">
-                                            <label htmlFor="exampleInputEmail1" className="form-label fs-6">Email address</label>
-                                            <input type="email" onBlur={getEmail} className="form-control registration-input-bg py-3 rounded-0" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="abc@pqr.xyz" required />
-                                            <div id="emailHelp" className="form-text text-dark">We'll never share your email with anyone else.</div>
-                                        </div>
-                                        <div className="mb-3">
-                                            <label htmlFor="inputPassword5" className="form-label fs-6">Password</label>
-                                            <input type="password" onBlur={getPassword} id="inputPassword5" className="form-control registration-input-bg py-3 rounded-0" aria-describedby="passwordHelpBlock" placeholder="Password" required />
-                                            <div id="passwordHelpBlock" className="form-text text-dark">
-                                                Your password must be at least 6 characters long, contain at least one lowercase letter and at least a number.
-                                            </div>
-                                        </div>
-                                        <div className="text-danger">{error}</div>
-                                        <div className="text-danger">{authError}</div>
-                                        <div className="text-center">
-                                            <button type="submit" className="btn btn-outline-dark rounded-0 mb-3">Register</button>
-                                            <p>Already have an account? <NavLink className="text-decoration-none text-dark account-switch ms-2" to="/login">Please Login</NavLink></p>
-                                        </div>
-                                    </form>
-                                    <hr className="mt-4" />
+                                    </div>
+                                    <div className="text-danger">{error}</div>
+                                    <div className="text-danger">{authError}</div>
                                     <div className="text-center">
-                                        <button onClick={handleGoogleLogin} className="mb-3 btn btn-outline-dark rounded-0"><p><i className="fab fa-google me-3"></i>Google Sign In</p></button>
+                                        <button type="submit" className="btn btn-outline-dark rounded-0 mb-3">Register</button>
+                                        <p>Already have an account? <NavLink className="text-decoration-none text-dark account-switch ms-2" to="/login">Please Login</NavLink></p>
                                     </div>
+                                </form>
+                                <hr className="mt-4" />
+                                <div className="text-center">
+                                    <button onClick={handleGoogleLogin} className="mb-3 btn btn-outline-dark rounded-0"><p><i className="fab fa-google me-3"></i>Google Sign In</p></button>
                                 </div>
                             </div>
-                            <div className="col-12 col-lg-6">
-                                <div className="d-flex justify-content-center">
-                                    <i className="fas fa-tools settings-icon"></i>
-                                </div>
+                        </div>
+                        <div className="col-12 col-lg-6">
+                            <div className="d-flex justify-content-center">
+                                <i className="fas fa-tools settings-icon"></i>
                             </div>
                         </div>
                     </div>
