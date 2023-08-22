91ad00e895df908e6f115386e0bf1e9ae69bf2e4
Avijit Roy <royavijit782@gmail.com>
2023-08-22 11:33:32 +0600

    card alignment changes

--- src/pages/Dashboard/ManageAllProducts/ManageAllProducts.js
+++ src/pages/Dashboard/ManageAllProducts/ManageAllProducts.js
@@ -44,23 +44,15 @@ const ManageAllProducts = () => {
                         products.map(product => (
                             <div className="col">
                                 <div className="card product-manage-all-products-page border-0 rounded-3 h-100">
-                                    <img src={product?.productImg} className="card-img-top rounded-3" alt="..." />
-                                    <div className="product-manage-all-products-page-name rounded-end">
-                                        <h5 className="card-title text-center m-0 py-2 text-white">{product?.productName}</h5>
-                                    </div>
+                                    <img src={product?.productImg} style={{ height: "250px" }} className="card-img-top rounded-3" alt="..." />
+                                    <h5 className="card-title product-manage-all-products-page-name rounded-end text-center m-0 py-2 text-white">{product?.productName}</h5>
                                     <div className="card-body">
-                                        <div className="card-details">
-                                            <h6 className="card-text text-black lh-base">{product?.short_description}</h6>
-                                        </div>
-                                    </div>
-                                    <div className="card-footer bg-transparent border-0 mb-3">
-                                        <h6 className="border-bottom border-2 border-dark pb-1">Reason To Buy</h6>
+                                        <p className="card-text text-black lh-base mb-3">{product?.short_description}</p>
+                                        <h5 className="border-bottom border-2 border-dark pb-1">Reason To Buy</h5>
                                         <p className="card-text text-black lh-base">{product?.reason}</p>
                                     </div>
                                     <div className="card-footer d-flex justify-content-center align-items-center py-3">
-                                        <div>
-                                            <h4 className="m-0">${product?.price}</h4>
-                                        </div>
+                                        <h4 className="m-0">${product?.price}</h4>
                                     </div>
                                     <div className="delete-btn">
                                         <button onClick={() => deleteBooking(product?._id)} className="btn rounded-3 action-btn-delete-manage-all-products-page"><h1><i className="fas fa-trash-alt fs-3 mx-3"></i></h1></button>
--- src/pages/Home/ProductsInHome/ProductsInHome.js
+++ src/pages/Home/ProductsInHome/ProductsInHome.js
@@ -7,23 +7,15 @@ const ProductsInHome = ({ product }) => {
     return (
         <div className="col">
             <div className="card products-in-home border-0 rounded-3 h-100">
-                <img src={productImg} className="card-img-top rounded-3" alt="..." />
-                <div className="products-in-home-name rounded-end">
-                    <h5 className="card-title text-center m-0 py-2 text-white">{productName}</h5>
-                </div>
+                <img src={productImg} style={{ height: "250px" }} className="card-img-top rounded-3" alt="..." />
+                <h5 className="card-title products-in-home-name rounded-end text-center m-0 py-2 text-white">{productName}</h5>
                 <div className="card-body">
-                    <div className="card-details">
-                        <h6 className="card-text text-black lh-base">{short_description}</h6>
-                    </div>
-                </div>
-                <div className="card-footer bg-transparent border-0 mb-3">
-                    <h6 className="border-bottom border-2 border-dark pb-1">Reason To Buy</h6>
+                    <p className="card-text text-black lh-base mb-3">{short_description}</p>
+                    <h5 className="border-bottom border-2 border-dark pb-1">Reason To Buy</h5>
                     <p className="card-text text-black lh-base">{reason}</p>
                 </div>
                 <div className="card-footer d-flex justify-content-between align-items-center py-3">
-                    <div>
-                        <h4 className="m-0">${price}</h4>
-                    </div>
+                    <h4 className="m-0">${price}</h4>
                     <NavLink to={`/purchase/${_id}`}><button className="btn btn-outline-dark rounded-3"><i className="fas fa-chevron-right"></i></button></NavLink>
                 </div>
             </div>
